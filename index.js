792ee0ea37966ec783e261c8132b1ce63e44f366
swapman972 <jordan.melidor@hotmail.fr>
2020-05-17 00:44:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter( (n) => {
+        return n.toLowerCase() === string.toLowerCase();
+    })
+}
+
+function fuzzyMatch(arr, str) {
+    return arr.filter( r => 
+        r.toLowerCase().indexOf(str.toLowerCase()) === 0
+    )
+}
+
+function matchName(array, string) {
+    return array.filter( obj => {
+        if (obj.name === string){ return obj }
+    })
+}
